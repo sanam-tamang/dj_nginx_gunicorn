c4cf4e4d1cf8ed52b37472fb6fa43b07f9fed605
sanam-tamang <snm.tmg7@gmail.com>
2023-04-24 15:27:29 +0545

    changes django 4.2 to 3.2.18

--- nginxtest/settings.py
+++ nginxtest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e9k!c4b3fyt@lz=jk9sow5*jt%+wsaaj2rn*aj1k&s04ib72b+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.254.169','https://dj-nginxandgunicorn.onrender.com']
 
